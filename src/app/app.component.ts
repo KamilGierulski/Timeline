ebf72e1187f6a0dee76e79a7bff2ea61313fd6ea
Kamil Gierulski <kamil.gierulski@gmail.com>
2021-02-28 19:58:54 +0100

    filtered duplicates and added animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,26 @@ import timeline from 'src/data.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'timeline';
-  public sortedtimeline = timeline.sort((a, b) => (a.data > b.data) ? 1 : -1);
-  public timelineObject:{
-    data: number,
-    title: string,
-    description: string,
-    icon: string
-  }[]=timeline; 
+  public sortedtimeline = timeline.sort((a, b) => (a.data > b.data ? 1 : -1));
+  removeDuplicates(timeline) {
+    var uniq = {};
+    var arrFiltered = timeline.filter(
+      (obj) => !uniq[obj.data] && (uniq[obj.data] = true)
+    );
+    return arrFiltered;
+  }
+  ngOnInit(): void {
+    this.timelineObject2 = this.removeDuplicates(this.timelineObject);
+  }
+  public timelineObject2 = [];
+  public timelineObject: {
+    data: number;
+    title: string;
+    description: string;
+    icon: string;
+  }[] = timeline;
 }
-
-
